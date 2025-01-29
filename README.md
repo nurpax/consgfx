eb5481acd18f845421749679cdc4eb44ddb2650c
Janne Hellsten <jjhellst@gmail.com>
2025-01-29 14:06:37 +0200

    CMake/nanobind builds

--- README.md
+++ README.md
@@ -6,3 +6,16 @@ source .venv\Scripts\activate.bat
 pip install git+https://github.com/wjakob/nanobind 'scikit-build-core[pyproject]'
 pip install --no-build-isolation -ve .
 ```
+
+### Configuring the libsixel build
+
+```
+meson setup --reconfigure \
+    -Dimg2sixel=enabled \
+    -Dgdk-pixbuf2=disabled \
+    -Dgd=disabled \
+    -Dlibcurl=disabled \
+    -Dpng=disabled \
+    -Djpeg=disabled \
+    build
+```
--- src/consgfx_ext.cpp
+++ src/consgfx_ext.cpp
@@ -1,8 +1,47 @@
 #include <nanobind/nanobind.h>
 
-int add(int a, int b) { return a + b; }
+#include <string.h>
 
-NB_MODULE(consgfx_ext, m) {
-    m.def("add", &add);
+#include "sixel.h"
+
+namespace nb = nanobind;
+
+struct Outbuf {
+    size_t size;
+    uint8_t* buf;
+};
+
+// Growing output buffer
+static int memory_write_callback(char *data, int size, void *priv) {
+    Outbuf* buf = (Outbuf*)priv;
+    buf->buf = (uint8_t*)realloc(buf->buf, buf->size + size);
+    memcpy(buf->buf + buf->size, data, size);
+    buf->size += size;
+    return size;
 }
 
+static nb::bytes sixel_encode_bytes(nb::bytes src_pixels, int width, int height) {
+    sixel_encoder_t *encoder;
+
+    sixel_dither_t* dither;
+    sixel_dither_new(&dither, -1, NULL); // no dither
+
+    Outbuf outbuf;
+    memset(&outbuf, 0, sizeof(outbuf));
+
+    sixel_output_t *output = NULL;
+    sixel_output_new(&output, memory_write_callback, (void *)&outbuf, NULL /* allocator */);
+
+    SIXELSTATUS status = sixel_encode((unsigned char*)src_pixels.c_str(), width, height, 3, dither, output);
+
+    sixel_output_unref(output);
+    sixel_dither_unref(dither);
+
+    auto res = nb::bytes(outbuf.buf, outbuf.size);
+    free(outbuf.buf);
+    return res;
+}
+
+NB_MODULE(consgfx_ext, m) {
+    m.def("sixel_encode_bytes", &sixel_encode_bytes);
+}
